e12d4a24625d53cd87770ae11c2abc23968ab95f
Poushali Guha <poushali.guha6@gmail.com>
2019-08-04 08:53:23 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # SystemLogger
+
+Whenever the system is being locked-unlocked, the duration will be recorded in a local file.
+
+## How to Use ##
+
+
+Dowload the source code. Run command prompt as Administartor. Go to 'v4.0.30319' folder. In most of the case the full path is C:\Windows\Microsoft.NET\Framework\v4.0.30319. In command promt execute the following command:
+
+
+### Install ###
+
+InstallUtil.exe "path to serivce.exe" + <<service name>> + ".exe"
+
+In my local system the path is
+
+"C:\Users\Poushali\Desktop\practice\SystemLogger\SystemLoggerService\bin\Release"
+
+Hence my command will look like
+
+InstallUtil.exe C:\Users\Poushali\Desktop\practice\SystemLogger\SystemLoggerService\bin\Release\SystemLoggerService.exe
+
+
+### Start/Stop Service ###
+
+Open the Run (Ctrl + R)
+
+Type sevice.msc
+
+Find your Service
+
+Right click on it to Start or Stop the Service.
+
+markdown
+### Uninstall ###
+
+
+InstallUtil.exe-u "path to serivce.exe" + <<service name>> + ".exe"
+
+In my local system the path is
+
+"C:\Users\Poushali\Desktop\practice\SystemLogger\SystemLoggerService\bin\Release"
+
+Hence my command will look like
+
+InstallUtil.exe-u C:\Users\Poushali\Desktop\practice\SystemLogger\SystemLoggerService\bin\Release\SystemLoggerService.exe
+
+
+### Output ###
+
+
+The default Output path is "C:\Windows". A text file will be created in this path, name of the file is  "SystemTimeLogger.txt"
+
+### Change Output File PAth ###
+
+If you want to change the path of the output file open the solution. Go to Service.cs file. Right click -> View Code.
+
+Go to ~OnSessionChange~  method. Change the file path. After this Rebuild and re install.
+
+
+### License ###
+
+
+his is under free license.
+
---
+++ SystemLoggerService/Service1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemLoggerService
+{
+    public partial class Service1 : ServiceBase
+    {
+        DateTime outTime = new DateTime();
+        public Service1()
+        {
+            InitializeComponent();
+            outTime = DateTime.Now;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+            this.Dispose();
+        }
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+
+            string filePath = @"C:\Windows\SystemTimeLogger.txt";
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamWriter sWriter = new StreamWriter(fs);
+
+            sWriter.BaseStream.Seek(0, SeekOrigin.End);
+            string activity = string.Empty;
+            string duration = string.Empty;
+            switch (changeDescription.Reason)
+            {
+                case SessionChangeReason.SessionLogon:
+                    duration = (DateTime.Parse(DateTime.Now.TimeOfDay.ToString()).Subtract(DateTime.Parse(outTime.TimeOfDay.ToString()))).ToString();
+
+                    activity = "Log On";
+                    break;
+                case SessionChangeReason.SessionLogoff:
+                    outTime = DateTime.Now;
+                    activity = "Log Off";
+                    break;
+                case SessionChangeReason.SessionLock:
+                    outTime = DateTime.Now;
+                    activity = "Lock ";
+                    break;
+                case SessionChangeReason.SessionUnlock:
+                    duration = (DateTime.Parse(DateTime.Now.TimeOfDay.ToString()).Subtract(DateTime.Parse(outTime.TimeOfDay.ToString()))).ToString();
+                    activity = "Unlock";
+                    break;
+
+                default:
+                    break;
+            }
+            sWriter.WriteLine(string.Format("{0} : {1}", activity, DateTime.Now.ToString()));
+            if (!string.IsNullOrEmpty(duration))
+            {
+                sWriter.WriteLine("Idle time : " + duration);
+                sWriter.WriteLine();
+            }
+            sWriter.Flush();
+            sWriter.Close();
+        }
+    }
+}
